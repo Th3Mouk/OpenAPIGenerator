6ea94750eb6409883b8020111112fee602d1eba9
Jérémy <marodon.jeremy@gmail.com>
2019-05-29 16:02:02 +0200

    :construction_worker: Improve CI with all PHP files

--- PathHelper.php
+++ PathHelper.php
@@ -1,10 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Th3Mouk\OpenAPIGenerator;
 
 class PathHelper
 {
-    const ROOT = '/specs';
-    const PATHS = self::ROOT.'/paths';
-    const DEFINITIONS = self::ROOT.'/definitions';
+    public const ROOT        = '/specs';
+    public const PATHS       = self::ROOT . '/paths';
+    public const DEFINITIONS = self::ROOT . '/definitions';
 }
--- functions.php
+++ functions.php
@@ -1,13 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 if (!function_exists('getRootPath')) {
+
     function getRootPath(): string
     {
         $dir = $rootDir = __DIR__;
-        while (!file_exists($dir.'/composer.lock')) {
-            $dir = \dirname($dir);
+        while (!file_exists($dir . '/composer.lock')) {
+            $dir = dirname($dir);
         }
 
         return $dir;
     }
+
 }
